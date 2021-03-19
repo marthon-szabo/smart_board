a69d3e3ec89a3ff65874b34de2dc2a3471eec46d
Zsani <horvathzsanett.95@gmail.com>
2021-03-19 12:19:32 +0100

    Added style to registration error messages

--- App/ClientApp/src/components/Register.js
+++ App/ClientApp/src/components/Register.js
@@ -1,9 +1,12 @@
-import React, { Component, useState, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import Modal from 'react-awesome-modal';
+import validate from './ValidateRegistrationInformation';
+import useForm from './UserRegistrationForm';
 import { RegisterStateContext } from "./RegisterStateContext";
 import { LoginStateContext } from "./LoginStateContext";
+import './Error.css';
 
-function Register() {
+function Register({ submitForm }) {
     const [registerState, setRegisterState] = useContext(RegisterStateContext);
     const [loginState, setLoginState] = useContext(LoginStateContext);
 
@@ -16,31 +19,68 @@ function Register() {
         setRegisterState(false);
     }
 
+    const { handleChange, handleSubmit, values, errors } = useForm(
+        submitForm,
+        validate
+    );
+
 
         return (
             <section>
-                <Modal className="register-modal" visible={registerState} style={{ background: "#fcd281" }} width="400" height="500" effect="fadeInLeft" onClickAway={() => closeRegisterWindow()}>
-                    <form style={{ padding: '5%'}}>
+                <Modal className="register-modal" visible={registerState} style={{ background: "#fcd281" }} width="400" height="550" effect="fadeInLeft" onClickAway={() => closeRegisterWindow()}>
+                    <form style={{ padding: '5%' }} onSubmit={handleSubmit}>
                         <h3>Sign Up</h3>
 
                         <div className="form-group">
                             <label>Username</label>
-                            <input type="text" className="form-control" placeholder="Enter username" />
+                            <input
+                                type="text"
+                                className="form-control"
+                                name='username'
+                                placeholder="Enter username"
+                                value={values.username}
+                                onChange={handleChange}
+                            />
+                            {errors.username && <p>{errors.username}</p>}
                         </div>
 
                         <div className="form-group">
                             <label>Email address</label>
-                            <input type="email" className="form-control" placeholder="Enter email" />
+                            <input
+                                type="text"
+                                name="email"
+                                className="form-control"
+                                placeholder="Enter email"
+                                value={values.email}
+                                onChange={handleChange}
+                            />
+                            {errors.email && <p>{errors.email}</p>}
                         </div>
 
                         <div className="form-group">
                             <label>Password</label>
-                            <input type="password" className="form-control" placeholder="Enter password" />
+                            <input
+                                type="password"
+                                className="form-control"
+                                name="password"
+                                placeholder="Enter password"
+                                value={values.password}
+                                onChange={handleChange}
+                            />
+                            {errors.password && <p>{errors.password}</p>}
                         </div>
 
                             <div class="form-group">
                             <label>Confirm Password</label>
-                            <input type="password" class="form-control" placeholder="Confirm Password " />
+                            <input
+                                type="password"
+                                class="form-control"
+                                name="password2"
+                                placeholder="Confirm Password "
+                                value={values.password2}
+                                onChange={handleChange}
+                            />
+                            {errors.password2 && <p>{errors.password2}</p>}
                         </div>
                             <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
                             <p className="forgot-password text-right">
