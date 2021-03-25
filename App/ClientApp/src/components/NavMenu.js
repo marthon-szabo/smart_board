c91459d8914ed6a416cae11b54869c4bff3b45ad
Zsani <horvathzsanett.95@gmail.com>
2021-03-25 10:37:44 +0100

    Added blurring effect to CSS

--- App/ClientApp/src/components/NavMenu.js
+++ App/ClientApp/src/components/NavMenu.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { LoginStateContext } from "./LoginStateContext";
 import { RegisterStateContext } from "./RegisterStateContext";
 import { CSRFTokenContext } from "./CSRFTokenContext";
@@ -11,9 +11,11 @@ function NavMenu() {
     const [loginState, setLoginState] = useContext(LoginStateContext);
     const [registrationState, setRegistrationState] = useContext(RegisterStateContext);
     const [token, setToken] = useContext(CSRFTokenContext);
+    const [className, setClassName] = useState("container");
 
     const openRegistrationWindow = () => {
         setRegistrationState(true);
+        setClassName("blurred-box");
     }
 
     const openLoginWindow = () => {
@@ -21,27 +23,34 @@ function NavMenu() {
         fetch('/user/token')
             .then(res => res.json())
             .then(data => setToken(data))
+        setClassName("blurred-box");
     }
 
         return (
-            
-        <header>
-            <div className="container-fluid main">
-                <div className="text-center main-text">
-                    <div className="c2a-btn footer-c2a-btn">
-                            <div className="btn-group btn-group-lg" role="group" aria-label="Call to action">
-                                <a type="button" class="btn btn-default btn-lg" href="#" onClick={() => openRegistrationWindow()}>Sign up</a>
-                            <span className="btn-circle btn-or">or</span>
-                                <a type="button" class="btn btn-default btn-lg" href="#" onClick={() => openLoginWindow()}>Sign in</a>
+            <><div className={ className }>
+                <header>
+                    <div className="container-fluid main">
+
+                        <div className="text-center main-text">
+                            <div className="c2a-btn footer-c2a-btn">
+                                <div className="btn-group btn-group-lg" role="group" aria-label="Call to action">
+                                    <a type="button" class="btn btn-default btn-lg" href="#" onClick={() => openRegistrationWindow()}>Sign up</a>
+                                    <span className="btn-circle btn-or">or</span>
+                                    <a type="button" class="btn btn-default btn-lg" href="#" onClick={() => openLoginWindow()}>Sign in</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                </div>
-
-                <Register/>
-                <Login/>
 
                 </header>
+            </div>
+                <div>
+                    <Register />
+                    <Login />
+                </div></>
+
+                
+            
             
     );
        
