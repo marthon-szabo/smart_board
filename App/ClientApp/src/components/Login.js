bdb1b515f5137208b1ef5d95c7906d71c9e9c6ba
Márton Szabó <makotoimx@gmail.com>
2021-03-25 17:40:45 +0100

    Commit checkmark picture
    In order to that the current state of the app be presentable.

--- App/ClientApp/src/components/Login.js
+++ App/ClientApp/src/components/Login.js
@@ -66,7 +66,8 @@ function Login({ submitForm }) {
                         </div>
 
                         <button id="login-btn" type="submit" className="btn btn-primary btn-block">
-                            <img src={check_mark} className="check-mark"></img>
+                            {/* <img src={check_mark} className="check-mark"></img> */}
+                            Sign in!
                         </button>
                         <p className="forgot-password text-right">
                         Don't have a  <a href="#" onClick={() => changeToRegistrationWindow()}>registration</a> yet?
