2bf3c92c922cedd7f35f2cff443cc60d0dd5d868
Zsani <horvathzsanett.95@gmail.com>
2021-03-25 10:37:04 +0100

    Created BlurStyleContext

---
+++ App/ClientApp/src/components/BlurStyleContext.js
@@ -0,0 +1,12 @@
+﻿import React, { useState, createContext } from 'react';
+
+export const BlurStyleContext = createContext();
+
+export const BlurStyleProvider = props => {
+    const [blurStyle, setBlurStyle] = useState("container");
+    return (
+        <BlurStateContext.Provider value={[blurStyle, setBlurStyle]}>
+            {props.children}
+        </BlurStateContext.Provider>
+    )
+}
