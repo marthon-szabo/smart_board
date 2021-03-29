e7f72fd2a1a6136c7c28496e4b3a04fd1ada6552
Márton Szabó <makotoimx@gmail.com>
2021-03-29 10:17:22 +0200

    Refactor user interaction checker

--- App/ClientApp/src/Utilities/UserInteracrtionChecker.js
+++ App/ClientApp/src/Utilities/UserInteracrtionChecker.js
@@ -2,24 +2,27 @@ import { removeSpinnerTextless } from '../Utilities/Spinner';
 import addCheckMark from '../Utilities/CheckMark';
 import check_mark from "../images/check_mark.png";
 import x_mark from "../images/x_mark.png";
+import addErrorMessage from '../Utilities/ErrorMessage';
 
 
 export const enableLogin = (button) => {
     removeSpinnerTextless(button);
     button.style.backgroundColor = "#32c671";
     addCheckMark(button, check_mark);
-    setTimeout(() => {
-        window.location.href = "https://localhost:5001/profile";
-    }, 1000);
-
 };
 
-export const disableLogin = (button, buttonText, alertText = 'Invalid username or password! Please try it again.') => {
+export const disableLogin = (button, buttonText, box, alertText) => {
     removeSpinnerTextless(button);
     const buttonColor = button.style.backgroundColor;
+    const errorMessage = document.querySelector(".error-message");
+    
     button.style.backgroundColor = "#f34336";
     addCheckMark(button, x_mark)
-    alert(alertText);
+
+    if (box.contains(errorMessage) == false) {
+        addErrorMessage(box, alertText);
+    }
+    
     setTimeout(() => {
         button.innerHTML = buttonText;
         button.style.backgroundColor = buttonColor;
