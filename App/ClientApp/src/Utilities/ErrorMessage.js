df8aa69b96ef4e1b6040d2996bf9c20ab6caa7c9
Márton Szabó <makotoimx@gmail.com>
2021-03-29 10:17:01 +0200

    Refactor error message utility

--- App/ClientApp/src/Utilities/ErrorMessage.js
+++ App/ClientApp/src/Utilities/ErrorMessage.js
@@ -2,8 +2,11 @@ const addErrorMessage = (element, content) => {
     const errorColor = "#f34336";
     const message = document.createElement("p");
 
+    message.className = "error-message";
     message.style.color = errorColor;
-    message.text = content;
+    message.innerText = `*${content}`;
+    message.style.fontSize = "1.125em";
+    message.style.fontStyle = "italic";
 
     element.appendChild(message);
 }
