3152fea4d7c914832c11616aa3a8d3fdc84fd881
Zsani <horvathzsanett.95@gmail.com>
2021-03-29 13:38:59 +0200

    Created UserDataContext to store profile data of logged in user

---
+++ App/ClientApp/src/components/contexts/UserDataContext.js
@@ -0,0 +1,13 @@
+﻿import React, { useState, createContext } from 'react';
+
+export const UserDataContext = createContext();
+
+export const UserDataProvider = props => {
+    const [UserDataState, setUserDaraState] = useState([]);
+
+    return (
+        <UserDataContext.Provider value={[UserDataState, setUserDataState]}>
+            {props.children}
+        </UserDataContext.Provider>
+    )
+}
