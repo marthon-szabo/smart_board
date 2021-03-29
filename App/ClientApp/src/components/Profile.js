138515bdec0e51cb48859c1524197281cbff9c75
Zsani <horvathzsanett.95@gmail.com>
2021-03-29 14:42:46 +0200

    Added personal data to profile page

--- App/ClientApp/src/components/Profile.js
+++ App/ClientApp/src/components/Profile.js
@@ -1,8 +1,21 @@
-﻿import React from 'react';
+﻿import React, { useContext } from 'react';
+import { UserDataContext } from "./contexts/UserDataContext";
 
 function Profile() {
+    const [userData, setUserData] = useContext(UserDataContext);
+    const username = userData.username;
+    const email = userData.email;
+    const takenQuests = userData.takenQuests ? userData.takenQuests : 0;
+    const doneQuests = userData.doneQuests ? userData.doneQuests : 0;
+    const badges = userData.badges ? userData.badges : 0;
     return (
-            <p> Here comes your profile data </p>
+        <div>
+            <p>Your username: {username}</p>
+            <p>Your email: {email}</p>
+            <p>Number of taken quests: {takenQuests} </p>
+            <p>Number of done quests: {doneQuests} </p>
+            <p>Number of your badges: {badges} </p>
+        </div>
         )
 }
 
