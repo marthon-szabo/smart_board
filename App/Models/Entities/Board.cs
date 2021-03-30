c85c466754c98a145505e9690d17aa4fddafb97a
Márton Szabó <makotoimx@gmail.com>
2021-03-30 11:14:13 +0200

    Fix table name for Board model

--- App/Models/Entities/Board.cs
+++ App/Models/Entities/Board.cs
@@ -2,6 +2,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace App.Models.Entities
 {
+    [Table("Boards")]
     public class Board
     {
         [Column("board_id")]
@@ -9,5 +10,8 @@ namespace App.Models.Entities
 
         [Column("board_name")]
         public string BoardName { get; set; }
+        
+        [Column("users_boards_id")]
+        public string UsersBoardsId { get; set; }
     }
 }
