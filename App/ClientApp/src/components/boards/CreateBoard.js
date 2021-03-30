5c0134b1983c12b6bbf21955fe57882eff4539ae
Zsani <horvathzsanett.95@gmail.com>
2021-03-30 16:51:41 +0200

    Moved style outside of the img tag

--- App/ClientApp/src/components/boards/CreateBoard.js
+++ App/ClientApp/src/components/boards/CreateBoard.js
@@ -3,11 +3,19 @@
 import PlusIcon from '../../images/plus.png';
 import './BoardCard.scss';
 
+const IconStyle = {
+    width: "80px",
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: "15px",
+    marginBottom: "15px"
+}
+
 function CreateBoard() {
     return (
         <div class="card-container center">
             <div class="card">
-                <img src={PlusIcon} alt="Plus icon" style={{ width: "80px", marginLeft: "auto", marginRight: "auto", marginTop: "15px", marginBottom:"15px" }}></img>
+                <img src={PlusIcon} alt="Plus icon" style={ IconStyle }></img>
                 <hr />
                 <p>Click the button to create a new board!</p>
                 <button>Create</button>
