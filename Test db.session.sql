f720caa7d59b30553697650fdfde39fecbdc872c
Márton Szabó <makotoimx@gmail.com>
2021-03-31 16:57:03 +0200

    Add sample queries for test cases
    If a test case won't run, the datas remain in the database. These queries delete them.

--- Test db.session.sql
+++ Test db.session.sql
@@ -8,3 +8,21 @@ CREATE TABLE users (
     password char(250),
     email char(250)
 );
+
+DROP TABLE IF EXISTS Boards;
+CREATE TABLE Boards (
+    board_id CHAR PRIMARY KEY,
+    board_name CHAR
+);
+
+DROP TABLE IF EXISTS Users_Boards;
+CREATE TABLE Users_Boards (
+    users_boards_id char PRIMARY KEY,
+    board_id CHAR NOT NULL,
+    user_id CHAR NOT NULL,
+    FOREIGN KEY(board_id) REFERENCES Boards(board_id),
+    FOREIGN KEY(user_id) REFERENCES Users(user_id)
+);
+-- INSERT INTO Boards VALUES('TestBoard', 'First Board');
+
+SELECT * FROM Boards;
