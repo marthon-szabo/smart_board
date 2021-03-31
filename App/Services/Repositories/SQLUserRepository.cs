becde76d6d09bc54ab887999563d3e383024cd71
Márton Szabó <makotoimx@gmail.com>
2021-03-31 16:58:55 +0200

    Fix GetUserByUsername()

--- App/Services/Repositories/SQLUserRepository.cs
+++ App/Services/Repositories/SQLUserRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using App.Models.Entities;
 using App.Services.Repositories.Interfaces;
 
@@ -17,7 +18,7 @@ namespace App.Services.Repositories
         {
             IEnumerable<User> users = this.GetAllEntities();
 
-            foreach (User user in users)
+            foreach (User user in users.ToArray())
             {
                 if(user.UserName.Equals(username))
                 {
