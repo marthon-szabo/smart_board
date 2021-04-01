ad8e162b6c85cc31accb87402c4748b7ddf829cb
Zsani <horvathzsanett.95@gmail.com>
2021-04-01 14:17:47 +0200

    Created get request to backend

--- App/Services/Repositories/SQLBoardRepository.cs
+++ App/Services/Repositories/SQLBoardRepository.cs
@@ -8,18 +8,21 @@ namespace App.Services.Repositories
     public class SQLBoardRepository : SQLRepositoryBase<Board>, IBoardRepository
     {
         private readonly IUsersBoardsRepository _UsersBoardsRepo;
+        private readonly IUserRepository _UserRepo;
 
-        public SQLBoardRepository(AppDbContext context, IUsersBoardsRepository usersBoardsRepo)
+        public SQLBoardRepository(AppDbContext context, IUsersBoardsRepository usersBoardsRepo,
+                                                        IUserRepository userRepo)
             : base(context)
         {
             _UsersBoardsRepo = usersBoardsRepo;
+            _UserRepo = userRepo;
         }
 
         public IEnumerable<Board> GetAllBoardsByUsername(string username)
         {
             IEnumerable<UsersBoards> usersBoards = _UsersBoardsRepo.GetAllEntities();
-
-            return usersBoards.Where(userBoard => username.Equals(userBoard.UserId)).Select(userBoard => userBoard.Board);
+            string id = _UserRepo.GetUserByUsername(username).UserId;
+            return usersBoards.Where(userBoard => id.Equals(userBoard.UserId)).Select(userBoard => userBoard.Board);
         }
     }
 }
