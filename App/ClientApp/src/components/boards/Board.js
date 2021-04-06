49d160accd6005578fe1f096126940374322337b
Zsani <horvathzsanett.95@gmail.com>
2021-04-06 16:31:15 +0200

    Added modal window content with html table

--- App/ClientApp/src/components/boards/Board.js
+++ App/ClientApp/src/components/boards/Board.js
@@ -1,5 +1,5 @@
-﻿import React from 'react';
-import BoardStateContext from "./BoardStateContext";
+﻿import React, { useContext } from 'react';
+import { BoardStateContext } from "../contexts/BoardStateContext";
 
 import BoardsIcon from '../../images/boards.png';
 import './BoardCard.scss';
@@ -12,15 +12,21 @@ const IconStyle = {
     marginBottom: "15px"
 }
 
-
 function Board(props) {
+    const [boardState, setBoardState] = useContext(BoardStateContext);
+
+    const openModalBoard = () => {
+        setBoardState(true);
+        document.querySelector(".container").classList.add("blurred-box");
+    }
+
     return (
         <div class="card-container center">
             <div class="card">
                 <img src={BoardsIcon} alt="Boards icon" style={ IconStyle }></img>
                 <hr />
                 <p>{props.board.boardName}</p>
-                <button>Open</button>
+                <button onClick={openModalBoard }>Open</button>
             </div>
         </div>
     )
--- App/ClientApp/src/components/boards/BoardDetails.js
+++ App/ClientApp/src/components/boards/BoardDetails.js
@@ -1,36 +1,82 @@
-﻿import React, { useEffect } from 'react';
+﻿import React, { useEffect, useContext } from 'react';
+import Modal from 'react-awesome-modal';
+import { BoardStateContext } from "../contexts/BoardStateContext";
+
+import "./TableStyle.css";
 
 const columnNames = [{
+    id: "asd",
     columnName: "New"
 },
-{
+    {
+    id: "123",
     columnName: "In progress"
 },
-{
+    {
+    id: "321",
     columnName: "Done"
 }
+]
+
+const taskNames = [{
+    id: "fdrg",
+    taskName: "First task",
+    columnId: "asd"
+},
+    {
+        id: "aaa",
+        taskName: "Second task",
+        columnId: "asd"
+    },
+    {
+        id: "1111",
+        taskName: "Third task",
+        columnId: "321"
+    }
 ]
 
 
 
 function BoardDetails() {
+    const [boardState, setBoardState] = useContext(BoardStateContext);
+
+    const closeModalWindow = () => {
+        setBoardState(false);
+        document.querySelector(".container.blurred-box").classList.remove("blurred-box");
+    }
+
     useEffect(() => {
         const listDiv = document.getElementById('columns-list');
-        const ul = document.createElement('ul');
+        const tr = document.createElement('tr');
         for (var i = 0; i < columnNames.length; ++i) {
-            const li = document.createElement('li');
-            li.innerHTML = columnNames[i].columnName;
-            ul.appendChild(li);
+            const th = document.createElement('th');
+            th.innerHTML = columnNames[i].columnName;
+            th.bgColor = "#726aa7";
+            th.id = columnNames[i].id;
+            tr.appendChild(th);
         }
-        listDiv.appendChild(ul);
+        listDiv.appendChild(tr);
+        for (var i = 0; i < taskNames.length; ++i) {
+            const tr = document.createElement('tr');
+            const td = document.createElement('td');
+            const thMatching = document.getElementById(taskNames[i].columnId);
+            td.innerHTML = taskNames[i].taskName;
+            td.id = taskNames[i].id;
+            tr.appendChild(td);
+            thMatching.appendChild(tr);
+        }
+        
     }, [])
 
     return (
-        <div>
-            <p>Board with columns will come here</p>
-            <div className="list-of-columns" id="columns-list">
-            </div>
-        </div>
+        <section>
+            <Modal className="create-modal" visible={boardState} width="800" height="600" effect="fadeInRight" onClickAway={() => closeModalWindow()}>
+                <div className="container">
+                    <table border="1" height="200" width="780" className="table-of-columns" id="columns-list">
+            </table>
+                </div>
+            </Modal>
+        </section >
     )
 }
 
