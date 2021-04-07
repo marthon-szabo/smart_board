1ace654d560be575bc53a4f43a15d1d008c0bdb0
Márton Szabó <makotoimx@gmail.com>
2021-04-07 11:36:48 +0200

    Refactor SQLRepositoryTestsBase

--- Tests/ServiceTests/Repositories/SQLBoardRepositoryTests.cs
+++ Tests/ServiceTests/Repositories/SQLBoardRepositoryTests.cs
@@ -20,35 +20,9 @@ namespace Tests
         {
             base.AdditionalSetupOperations = () => 
             {
-                base.Database.ExecuteSqlRaw("INSERT INTO UsersBoards VALUES('TestUsersBoards', 'TestBoard1', 'TestUser1')");
                 base.Database.ExecuteSqlRaw("INSERT INTO users (user_id, username, password, email) VALUES('TestUser1', 'Dummy User', 'test', 'a@c.c')");
-            };
-
-            base.AdditionalTearDownOperations = () =>
-            {
-                base.Database.ExecuteSqlRaw(@"
-                    DROP TABLE IF EXISTS Users_Boards;
-                    CREATE TABLE Users_Boards (
-                        users_boards_id char PRIMARY KEY,
-                        board_id CHAR NOT NULL,
-                        user_id CHAR NOT NULL,
-                        FOREIGN KEY(board_id) REFERENCES Boards(board_id),
-                        FOREIGN KEY(user_id) REFERENCES Users(user_id)
-                    );
-                ");
-
-                base.Database.ExecuteSqlRaw(@"
-                    DROP TABLE IF EXISTS users;
-                    CREATE TABLE users (
-                        user_id TEXT PRIMARY KEY,
-                        username char(50),
-                        badges char,
-                        done_quests char,
-                        taken_quests char,
-                        password char(250),
-                        email char(250)
-                    );
-                ");
+                base.Database.ExecuteSqlRaw("INSERT INTO Boards VALUES('TestBoard1', 'Test Board 1')");
+                base.Database.ExecuteSqlRaw("INSERT INTO Users_Boards VALUES('TestUsersBoards', 'TestBoard1', 'TestUser1')");
             };
         }
 
@@ -56,7 +30,7 @@ namespace Tests
         public void GetAllBoardsByUsername_Username_ReturnsBoards()
         {
             // Arrange
-            string expected = "TestBoard1";
+            string expected = "Test Board 1";
 
             // Act
             string result = base._repo.GetAllBoardsByUsername("Dummy User").ToArray()[0].BoardName;
--- Tests/ServiceTests/Repositories/SQLRepositoryTestsBase.cs
+++ Tests/ServiceTests/Repositories/SQLRepositoryTestsBase.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using App.Models.Entities;
+using App.Services.Repositories;
 using App.Services.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
@@ -18,14 +19,13 @@ namespace Tests
         protected TRepo _repo;
 
         protected Action AdditionalSetupOperations { get; set; }
-        protected Action AdditionalTearDownOperations { get; set; }
 
         public SQLRepositoryTestsBase(IEnumerable<string> seedValues = null) : base(new DbContextOptionsBuilder<AppDbContext>()
                 .UseSqlite(DbIntegrationTester.GetConnection())
                 .Options)
         {
             _seedValues = seedValues;
-            _repo = (TRepo)Activator.CreateInstance(typeof(TRepo), this);
+            _repo = (TRepo)Activator.CreateInstance(typeof(TRepo), this, new SQLUsersBoardsRepository(this), new SQLUserRepository(this));
             _integrationTester = new GeneralIntegra<TRepo, TEntity>((IGeneralRepository<TEntity>)_repo, seedValues);
         }
 
@@ -43,12 +43,38 @@ namespace Tests
         [TearDown]
         protected void TearDown()
         {
-            _integrationTester.DropTable(this);
 
-            if (AdditionalTearDownOperations != null)
-            {
-                AdditionalTearDownOperations();
-            }
+            base.Database.ExecuteSqlRaw(@"
+                    DROP TABLE IF EXISTS Users_Boards;
+                    CREATE TABLE Users_Boards (
+                        users_boards_id char PRIMARY KEY,
+                        board_id CHAR NOT NULL,
+                        user_id CHAR NOT NULL,
+                        FOREIGN KEY(board_id) REFERENCES Boards(board_id),
+                        FOREIGN KEY(user_id) REFERENCES Users(user_id)
+                    );
+                ");
+
+                base.Database.ExecuteSqlRaw(@"
+                    DROP TABLE IF EXISTS users;
+                    CREATE TABLE users (
+                        user_id TEXT PRIMARY KEY,
+                        username char(50),
+                        badges char,
+                        done_quests char,
+                        taken_quests char,
+                        password char(250),
+                        email char(250)
+                    );
+                ");
+
+                base.Database.ExecuteSqlRaw(@"
+                    DROP TABLE IF EXISTS Boards;
+                    CREATE TABLE Boards (
+                        board_id CHAR PRIMARY KEY,
+                        board_name CHAR
+                    );
+                ");
         }
     }
 }
