c15abb9cd0b91a8f09c2d268b86a87f91a646ed2
Zsani <horvathzsanett.95@gmail.com>
2021-04-08 16:44:00 +0200

    Created route for creating new boards

--- App/Controllers/BoardController.cs
+++ App/Controllers/BoardController.cs
@@ -1,8 +1,11 @@
-using System;
 using System.Collections.Generic;
+using System.IO;
 using App.Models.Entities;
+using App.Models.ViewModels;
 using App.Services.Repositories.Interfaces;
+using App.Utilities;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace App.Controllers
 {
@@ -20,6 +23,35 @@ namespace App.Controllers
         public IEnumerable<Board> GetAllBoards(string userName)
         {
             return _boardRepo.GetAllBoardsByUsername(userName);
+        }
+
+        [HttpPost("boards/create-board")]
+        public IEnumerable<Board> CreateNewBoard()
+        {
+            Stream stream = Request.Body;
+
+            NewBoardVM newBoardVM = this.ReadRequestBody<NewBoardVM>(stream);
+
+            string id = IdGenerator.GenerateId();
+
+            Board newBoard = new Board
+            {
+                BoardId = id,
+                BoardName = newBoardVM.BoardName
+            };
+
+            _boardRepo.CreateEntity(newBoard);
+            return _boardRepo.GetAllBoardsByUsername(newBoardVM.UserName);
+        }
+
+        private T ReadRequestBody<T>(Stream stream)
+        {
+            StreamReader sr = new StreamReader(stream);
+            string requestJson = sr.ReadToEndAsync().Result;
+
+            T requestVM = JsonConvert.DeserializeObject<T>(requestJson);
+
+            return requestVM;
         }
     }
 }
