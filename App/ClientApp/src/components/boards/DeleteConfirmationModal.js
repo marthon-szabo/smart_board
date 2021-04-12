f4b4c0021929b5181933e007b837685ff7bafe7d
Zsani <horvathzsanett.95@gmail.com>
2021-04-12 10:59:11 +0200

    Added main functionality of DeleteModal

--- App/ClientApp/src/components/boards/DeleteConfirmationModal.js
+++ App/ClientApp/src/components/boards/DeleteConfirmationModal.js
@@ -3,10 +3,31 @@ import Modal from 'react-awesome-modal';
 import { DeleteModalContext } from "../contexts/DeleteModalContext";
 import { UserDataContext } from "../contexts/UserDataContext";
 
+import DeleteIcon from '../../images/delete.png';
+
 
 function DeleteConfirmationModal() {
+
+    const [deleteBoardState, setDeleteBoardState] = useContext(DeleteModalContext);
+
+    const IconStyle = {
+        width: "100px",
+        marginLeft: "auto",
+        marginRight: "auto",
+        marginTop: "15px",
+        marginBottom: "15px"
+    }
+
+    const closeModalWindow = () => {
+        setDeleteBoardState(false);
+        document.querySelector(".container.blurred-box").classList.remove("blurred-box");
+    }
+
     return (
         <section>
+            <Modal className="create-modal" visible={deleteBoardState} width="400" height="350" effect="fadeInDown" onClickAway={() => closeModalWindow()}>
+                <img src={DeleteIcon} alt="Plus icon" style={IconStyle}></img>
+            </Modal>
         </section>
         )
 }
