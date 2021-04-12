a0e86b715aa6245674af629370c2172ee73a9038
Zsani <horvathzsanett.95@gmail.com>
2021-04-12 10:58:36 +0200

    Added component for Landing page

--- App/ClientApp/src/components/LandingPage.js
+++ App/ClientApp/src/components/LandingPage.js
@@ -5,6 +5,7 @@ import CreateBoardModal from "./boards/CreateBoardModal";
 import BoardDetails from "./boards/BoardDetails";
 
 import './NavMenu.css';
+import DeleteConfirmationModal from './boards/DeleteConfirmationModal';
 
 function LandingPage() {
     return (
@@ -13,6 +14,7 @@ function LandingPage() {
             <LandingHeader/>
             </div>
             <CreateBoardModal />
+            <DeleteConfirmationModal/>
             <BoardDetails/>
         </div>
         )
