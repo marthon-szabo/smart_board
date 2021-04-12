1ade847a66d4585199b52edb826f83d01d0b51fb
Zsani <horvathzsanett.95@gmail.com>
2021-04-12 10:39:36 +0200

    Created component for deleting an existing board from database

---
+++ App/ClientApp/src/components/boards/DeleteConfirmationModal.js
@@ -0,0 +1,14 @@
+﻿import React, { useContext } from 'react';
+import Modal from 'react-awesome-modal';
+import { DeleteModalContext } from "../contexts/DeleteModalContext";
+import { UserDataContext } from "../contexts/UserDataContext";
+
+
+function DeleteConfirmationModal() {
+    return (
+        <section>
+        </section>
+        )
+}
+
+export default DeleteConfirmationModal;
