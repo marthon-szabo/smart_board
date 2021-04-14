27bcaab433f4088fb356ce55fe3ab9d8044cbd57
Márton Szabó <makotoimx@gmail.com>
2021-04-14 14:01:09 +0200

    Connect board controller test into test db service

--- Tests/ControllerTests/BoardControllerTests.cs
+++ Tests/ControllerTests/BoardControllerTests.cs
@@ -5,33 +5,41 @@ using App.Models.Entities;
 using App.Services.Repositories.Interfaces;
 using NSubstitute;
 using NUnit.Framework;
+using App.Services.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace Tests
 {
-    public class BoardControllerTests
+    public class BoardControllerTests : SQLRepositoryTestsBase<SQLBoardRepository, Board>
     {
+
+        private readonly SQLBoardRepository _boardRepo;
+
+        public BoardControllerTests()
+        {
+            base.AdditionalSetupOperations = () => 
+            {
+                base.Database.ExecuteSqlRaw("INSERT INTO Boards VALUES('TestBoard', 'First Board')");
+                base.Database.ExecuteSqlRaw("INSERT INTO users (user_id, username, password, email) VALUES('TestUser', 'Dummy User', 'test', 'a@c.c')");
+                base.Database.ExecuteSqlRaw("INSERT INTO Users_Boards VALUES('UBTest', 'TestBoard', 'TestUser')");
+            };
+        }
+
         [Test]
         public void GetAllBoards_Username_ReturnsBoards()
         {
             // Arrange
-            string expected = "Test board";
+            string expected = "First Board";
             
-            IEnumerable<Board> dummyBoards = new Board[]
-            {
-                new Board
-                {
-                    BoardId = "TestId",
-                    BoardName = "Test board"
-                }
-            };
+            
 
-            IBoardRepository stubBoardRepo = Substitute.For<IBoardRepository>();
-            stubBoardRepo.GetAllBoardsByUsername("test").Returns(dummyBoards);
+            IUserRepository stubUserRepo = Substitute.For<IUserRepository>();
+            IUsersBoardsRepository stubUsersBoardsRepo = Substitute.For<IUsersBoardsRepository>();
 
-            BoardController bC = new BoardController(stubBoardRepo);
+            BoardController bC = new BoardController(base._repo, stubUserRepo, stubUsersBoardsRepo);
 
             // Act
-            string result = bC.GetAllBoards("test").ToArray()[0].BoardName;
+            string result = bC.GetAllBoards("Dummy User").ToArray()[0].BoardName;
 
             // Assert
             Assert.AreEqual(expected, result);
