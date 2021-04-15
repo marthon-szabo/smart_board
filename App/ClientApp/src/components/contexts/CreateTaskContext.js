81ea8ddf6c21cfe253cb94387fc724f9c2165400
Zsani <horvathzsanett.95@gmail.com>
2021-04-15 12:00:11 +0200

    Created context file to open task adding window

---
+++ App/ClientApp/src/components/contexts/CreateTaskContext.js
@@ -0,0 +1,13 @@
+﻿import React, { useState, createContext } from 'react';
+
+export const CreateTaskContext = createContext();
+
+export const CreateTaskProvider = props => {
+    const [createTaskState, setCreateTaskState] = useState(false);
+
+    return (
+        <CreateTaskContext.Provider value={[createTaskState, setCreateTaskState]}>
+            {props.children}
+        </CreateTaskContext.Provider>
+    )
+}
