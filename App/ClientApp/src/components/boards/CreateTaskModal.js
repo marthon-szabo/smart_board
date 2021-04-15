2ce95e3cbe86c667f133ee3a989a4c03cad7817e
Zsani <horvathzsanett.95@gmail.com>
2021-04-15 14:04:44 +0200

    Added form with multiple input fields to the modal component

---
+++ App/ClientApp/src/components/boards/CreateTaskModal.js
@@ -0,0 +1,45 @@
+﻿import React, { useContext } from 'react';
+import Modal from 'react-awesome-modal';
+import { CreateTaskContext } from "../contexts/CreateTaskContext";
+
+function CreateTaskModal() {
+
+    const [openState, setOpenState] = useContext(CreateTaskContext);
+
+    const closeModalWindow = () => {
+        setOpenState("");
+    }
+
+    return (
+        <section>
+            <Modal className="create-modal" visible={openState.length == 0 ? false : true} width="400" height="365" effect="fadeInDown" onClickAway={() => closeModalWindow()}>
+                <form id="create-task-form" style={{ padding: '5%' }}>
+                    <div className="container">
+                        <div className="create-task-head">
+                            <h3>Create a task for <strong>{openState}</strong></h3>
+                        </div>
+                        <div className="form-group">
+                            <label>Task name:</label>
+                            <input
+                                id="task-name"
+                                type="text"
+                                className="form-control"
+                                name='board-name'
+                                placeholder="Enter task's name"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label>Description:</label>
+                            <textarea id="task-description" style={{ height: "105px" }} className="form-control" name='Description' placeholder="Write a description here"/>
+                        </div>
+                        <button id="task-btn" type="submit" className="btn btn-primary btn-block">
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </Modal>
+        </section>
+        )
+}
+
+export default CreateTaskModal;
