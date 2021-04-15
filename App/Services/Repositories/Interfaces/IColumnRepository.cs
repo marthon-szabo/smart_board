d98522c204b9e7cdf99f76a88c798445807ba878
Márton Szabó <makotoimx@gmail.com>
2021-04-15 16:42:17 +0200

    Add get and create methods

--- App/Services/Repositories/Interfaces/IColumnRepository.cs
+++ App/Services/Repositories/Interfaces/IColumnRepository.cs
@@ -1,9 +1,12 @@
+using System.Collections.Generic;
 using App.Models.Entities;
+using App.Models.ViewModels;
 
 namespace App.Services.Repositories.Interfaces
 {
     public interface IColumnRepository : IGeneralRepository<Column>
     {
-         
+         IEnumerable<Column> GetColumnsByColumnVM(ColumnVM columnVM, IBoardRepository boardRepo);
+         Column CreatColumnByColumnVM(ColumnVM columnVM, IBoardRepository boardRepo);
     }
 }
--- App/Services/Repositories/SQLColumnRepository.cs
+++ App/Services/Repositories/SQLColumnRepository.cs
@@ -1,5 +1,10 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
 using App.Models.Entities;
+using App.Models.ViewModels;
 using App.Services.Repositories.Interfaces;
+using App.Utilities;
 
 namespace App.Services.Repositories
 {
@@ -8,6 +13,32 @@ namespace App.Services.Repositories
         public SQLColumnRepository(AppDbContext context)
             : base(context)
         {
+        }
+
+        public Column CreatColumnByColumnVM(ColumnVM columnVM,  IBoardRepository boardRepo)
+        {
+            Board board = this.GetBoard(columnVM, boardRepo);
+
+            Column newColumn = new Column
+            {
+                Id = IdGenerator.GenerateId(),
+                BoardId = board.BoardId,
+                Name = columnVM.ColumnName
+            };
+
+            return newColumn;
+        }
+
+        public IEnumerable<Column> GetColumnsByColumnVM(ColumnVM columnVM, IBoardRepository boardRepo)
+        {
+            Board board = this.GetBoard(columnVM, boardRepo);
+            
+            return base.GetAllEntities().Select(col => col).Where(col => col.BoardId.Equals(board.BoardId));
+        }
+
+        private Board GetBoard(ColumnVM columnVM, IBoardRepository boardRepo)
+        {
+            return boardRepo.GetBoardByBoardName(columnVM.BoardName);
         }
     }
 }
