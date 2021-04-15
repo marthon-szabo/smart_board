0f28fec5a625acb71998a1ea5226d721ac51207a
Zsani <horvathzsanett.95@gmail.com>
2021-04-15 10:21:40 +0200

    Created context for delete confirmation modal state storing

---
+++ App/ClientApp/src/components/contexts/DeleteConfirmationContext.js
@@ -0,0 +1,13 @@
+﻿import React, { useState, createContext } from 'react';
+
+export const DeleteConfirmationContext = createContext();
+
+export const DeleteConfirmationProvider = props => {
+    const [deleteConfirmationState, setDeleteConfirmationState] = useState("");
+
+    return (
+        <DeleteConfirmationContext.Provider value={[deleteConfirmationState, setDeleteConfirmationState]}>
+            {props.children}
+        </DeleteConfirmationContext.Provider>
+    )
+}
