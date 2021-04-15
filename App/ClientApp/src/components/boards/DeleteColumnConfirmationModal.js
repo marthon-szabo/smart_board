7d7fbf77bd4af8f4f5f04d50316c23f6675fef0b
Zsani <horvathzsanett.95@gmail.com>
2021-04-15 10:22:17 +0200

    Added component with styling for accepting deletion of a column

---
+++ App/ClientApp/src/components/boards/DeleteColumnConfirmationModal.js
@@ -0,0 +1,46 @@
+﻿import React, { useContext, useState } from 'react';
+import Modal from 'react-awesome-modal';
+import { DeleteConfirmationContext } from "../contexts/DeleteConfirmationContext";
+import { BoardStateContext } from "../contexts/BoardStateContext";
+
+import DeleteIcon from '../../images/delete.png';
+
+
+function DeleteColumnConfirmationModal() {
+
+    const [openState, setOpenState] = useContext(DeleteConfirmationContext);
+    const [boardName, setBoardName] = useContext(BoardStateContext);
+
+    const closeModalWindow = () => {
+        setOpenState("");
+    }
+
+    const IconStyle = {
+        display: "block",
+        width: "80px",
+        marginLeft: "auto",
+        marginRight: "auto",
+        marginTop: "15px",
+        marginBottom: "15px",
+        alignContent: "center",
+        verticalAlign: "middle"
+    }
+
+return (
+    <section>
+        <Modal className="delete-column-modal" visible={openState.length == 0 ? false : true} width="400" height="335" effect="fadeInDown" onClickAway={() => closeModalWindow()}>
+            <div style={{ padding: "0px 25px" }}>
+                <img src={DeleteIcon} alt="delete icon" style={ IconStyle }></img>
+            <p>Are you sure you want to delete this column?</p>
+            <p>Column name: <strong>{openState}</strong> </p>
+            <p>From this table: <strong>{boardName}</strong> </p>
+            <button style={{
+                backgroundColor: "#f34336", borderColor: "#f34336", border: "1px solid transparent", padding: "0.375rem 0.75rem" }} id="delete-btn" type="submit" className="btn btn-primary btn-block">Yes</button>
+                <button id="cancel-btn" type="submit" className="btn btn-primary btn-block">Cancel</button>
+                </div>
+        </Modal>
+    </section>
+)
+}
+
+export default DeleteColumnConfirmationModal;
