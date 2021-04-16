59f3ee0ef9913b5dead0cdb70dc40a11c69481ed
Zsani <horvathzsanett.95@gmail.com>
2021-04-16 09:58:57 +0200

    Added a calendar to the task creating component

--- App/ClientApp/src/components/boards/CreateTaskModal.js
+++ App/ClientApp/src/components/boards/CreateTaskModal.js
@@ -1,10 +1,15 @@
-﻿import React, { useContext } from 'react';
+﻿import React, { useContext, useState } from 'react';
 import Modal from 'react-awesome-modal';
+import Calendar from 'react-calendar';
 import { CreateTaskContext } from "../contexts/CreateTaskContext";
+
+import 'react-calendar/dist/Calendar.css';
+import './CalendarStyle.css';
 
 function CreateTaskModal() {
 
     const [openState, setOpenState] = useContext(CreateTaskContext);
+    const [value, onChange] = useState(new Date());
 
     const closeModalWindow = () => {
         setOpenState("");
@@ -12,11 +17,19 @@ function CreateTaskModal() {
 
     return (
         <section>
-            <Modal className="create-modal" visible={openState.length == 0 ? false : true} width="400" height="365" effect="fadeInDown" onClickAway={() => closeModalWindow()}>
+            <Modal className="create-modal" visible={openState.length == 0 ? false : true} width="400" height="670" effect="fadeInDown" onClickAway={() => closeModalWindow()}>
+                
                 <form id="create-task-form" style={{ padding: '5%' }}>
                     <div className="container">
                         <div className="create-task-head">
                             <h3>Create a task for <strong>{openState}</strong></h3>
+                        </div>
+                        <div className="form-group">
+                            <label>Deadline:</label>
+                            <Calendar
+                                onChange={onChange}
+                                value={value}
+                            />
                         </div>
                         <div className="form-group">
                             <label>Task name:</label>
