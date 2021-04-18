a3def88e78b8ca20ac924d3655d01d72244afd3a
Zsani <horvathzsanett.95@gmail.com>
2021-04-18 18:44:14 +0200

    Deleted unnecessarry code parts

--- App/ClientApp/src/components/boards/BoardDetails.js
+++ App/ClientApp/src/components/boards/BoardDetails.js
@@ -1,43 +1,32 @@
-﻿import React, { useContext, useState, useEffect } from 'react';
+﻿import React, { useContext, useState } from 'react';
 import { BoardStateContext } from "../contexts/BoardStateContext";
 import { CreateColumnContext } from "../contexts/CreateColumnContext";
-import { CreateTaskContext } from "../contexts/CreateTaskContext";
-import { DeleteColumnConfirmationContext } from "../contexts/DeleteColumnConfirmationContext";
-import { DeleteTaskConfirmationContext } from "../contexts/DeleteTaskConfirmationContext";
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { DragDropContext } from 'react-beautiful-dnd';
 import BoardModal from './BoardModalComponents/BoardModal';
-import { ColumnsContext } from '../contexts/ColumnsContext';
+import Columns from '../columns/Columns';
 
 import "./TableStyle.css";
 import PlusIcon from "../../images/plus-green.png";
-import DeleteIcon from "../../images/deleteRed.png";
-import DeleteRed from "../../images/delete-red-background.png";
 
 const taskNames = [{
     id: "fdrg",
     taskName: "First task",
     columnId: "asd"
 },
-    {
-        id: "aaa",
-        taskName: "Second task",
-        columnId: "asd"
-    },
-    {
-        id: "1111",
-        taskName: "Third task",
-        columnId: "321"
-    }
-]
+{
+    id: "aaa",
+    taskName: "Second task",
+    columnId: "asd"
+},
+{
+    id: "1111",
+    taskName: "Third task",
+    columnId: "321"
+}]
 
 function BoardDetails() {
     const [boardState, setBoardState] = useContext(BoardStateContext);
-    const [columns, setColumns] = useContext(ColumnsContext);
-    const [tasks, setTasks] = useState([]);
-    const [deleteColState, setDeleteColState] = useContext(DeleteColumnConfirmationContext);
-    const [deleteTaskState, setDeleteTaskState] = useContext(DeleteTaskConfirmationContext);
     const [openColumnState, setOpenColumnState] = useContext(CreateColumnContext);
-    const [openTaskState, setOpenTaskState] = useContext(CreateTaskContext);
     const [taskId, setTaskId] = useState(null);
 
     const closeModalWindow = () => {
@@ -45,10 +34,6 @@ function BoardDetails() {
         document.querySelector(".container.blurred-box").classList.remove("blurred-box");
     }
 
-    const handleClick = (e, data) => {
-        console.log(data);
-        setTaskId(data);
-    }
 
     const onDragEnd = result => {
         const { source, destination } = result;
@@ -70,52 +55,8 @@ function BoardDetails() {
         }
     }
 
-    function getElement(elemType, taskId) {
-        return document.getElementById(elemType + taskId);
-    }
-
-    const showHiddenElements =  (e, id) => {
-        const elemToShowAdd = getElement("addButton-", id);
-        elemToShowAdd.classList.remove("hidden");
-        const elemToShowDelete = getElement("removeButton-", id);
-        elemToShowDelete.classList.remove("hidden");
-    }
-
-    const hideHiddenElements = (e, id) => {
-        const elemToShowAdd = getElement("addButton-", id);
-        elemToShowAdd.classList.add("hidden");
-        const elemToShowDelete = getElement("removeButton-", id);
-        elemToShowDelete.classList.add("hidden");
-    }
-
-    const showHiddenElementsTask = (e, id) => {
-        const elemToShowAdd = getElement("removeButton-", id);
-        elemToShowAdd.classList.remove("hidden");
-    }
-
-    const hideHiddenElementsTask = (e, id) => {
-        const elemToShowAdd = getElement("removeButton-", id);
-        elemToShowAdd.classList.add("hidden");
-    }
-
-    const openDeleteColumnModal = (columnName) => {
-        setDeleteColState(columnName);
-    }
-
-    const openDeleteTaskModal = (columnName, taskName) => {
-        const taskObj = {
-            columnName: columnName,
-            taskName: taskName
-        }
-        setDeleteTaskState(taskObj);
-    }
-
     const openAddColumnModal = () => {
         setOpenColumnState(true);
-    }
-
-    const openAddTaskModal = (columnName) => {
-        setOpenTaskState(columnName);
     }
 
     return (
@@ -127,55 +68,8 @@ function BoardDetails() {
                     </div>
                     <DragDropContext onDragEnd={onDragEnd}>
                         <div width="780" className="table-of-columns" id="columns-list">
-                            {
-                                columns.map((item) => (
-                                    <Droppable droppableId={ item.id}>
-                            {(provided) => (
-                                <div {...provided.droppableProps}
-                                    ref={provided.innerRef}>
-                                                <div className="board-column" id={item.id} onMouseEnter={(e) => showHiddenElements(e, item.id)} onMouseLeave={(e) => hideHiddenElements(e, item.id)} style={{ margin: "5px" }}>
-                                                    <div className="board-header" style={{position:"relative"}}>
-                                                        <img className="remove-column-button hidden"
-                                                            id={"removeButton-" + item.id}
-                                                            src={DeleteRed} alt="delete icon"
-                                                            title="Click here to delete this column"
-                                                            onClick={() => openDeleteColumnModal(item.name) }
-                                                        ></img>
-                                                    <div className="board-title">
-                                                        {item.name}
-                                                        </div>
-                                                        </div>
-                                                    {taskNames.map((taskItem, index) => (
-                                                            taskItem.columnId === item.id && (
-                                                                <div id={taskItem.id} onMouseDown={((e) => handleClick(e, taskItem.id))}>
-                                                            <Draggable key={taskItem.id} draggableId={taskItem.id} index={index} >
-                                                                {(provided) => (
-                                                                            <div className="content-div" onMouseEnter={(e) => showHiddenElementsTask(e, taskItem.id)} onMouseLeave={(e) => hideHiddenElementsTask(e, taskItem.id)}
-                                                                    ref={provided.innerRef}
-                                                                    {...provided.draggableProps}
-                                                                    {...provided.dragHandleProps}>
-                                                                                <div style={{ position: "relative" }}>
-                                                                                    <img className="remove-task-button hidden" id={"removeButton-" + taskItem.id} src={DeleteIcon} alt="delete icon" title="Click here to delete this task" onClick={() => openDeleteTaskModal(item.columnName, taskItem.taskName)}></img>
-                                                                                    {taskItem.taskName}
-                                                                                </div>
-                                                                    </div>
-                                                                )}
-                                                                    </Draggable>
-                                                            </div>
-                                                        )))
-                                                    }<img className="add-image hidden"
-                                                        id={"addButton-" + item.id}
-                                                        src={PlusIcon}
-                                                        alt="plus icon"
-                                                        title="Click here to add a new task"
-                                                        onClick={() => openAddTaskModal(item.columnName)} 
-                                                    >
-                                                    </img>{provided.placeholder}
-                                            </div>
-                                </div>
-                            )}
-                                    </Droppable>
-                                ))}
+                            <Columns/>
+
                             <img src={PlusIcon} alt="plus icon" style={{ height: "30px", display: "block", marginTop: "5px" }} title="Click here to add a new column" onClick={() => openAddColumnModal()}></img>
                         </div>
                     </DragDropContext>
