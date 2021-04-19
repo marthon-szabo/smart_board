f7988bcfe16b9c719dbf0e052fe4171c1cf7eaf8
Márton Szabó <makotoimx@gmail.com>
2021-04-19 14:11:01 +0200

    Create task controller

--- App/Controllers/TaskController.cs
+++ App/Controllers/TaskController.cs
@@ -17,7 +17,7 @@ namespace App.Controllers
         [HttpGet("boards/{columnName}/tasks")]
         public void GetAllEntitiesByColumnName(string columnName)
         {
-
+            
         }
     }
 }
