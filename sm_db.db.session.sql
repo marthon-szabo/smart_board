661917dcf65826bc023d251d856f4763fca7e3bc
Márton Szabó <makotoimx@gmail.com>
2021-04-19 13:43:01 +0200

    Add Task query to sql files

--- sm_db.db.session.sql
+++ sm_db.db.session.sql
@@ -1,18 +1,30 @@
--- CREATE TABLE Boards (
---     board_id CHAR PRIMARY KEY,
---     board_name CHAR,
---     users_boards_id CHAR NOT NULL,
---     FOREIGN KEY(users_boards_id) REFERENCES Users_Boards(users_boards_id)
--- );
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    user_id TEXT PRIMARY KEY,
+    username char(50),
+    badges char,
+    done_quests char,
+    taken_quests char,
+    password char(250),
+    email char(250)
+);
 
--- CREATE TABLE Users_Boards (
---     users_boards_id CHAR PRIMARY KEY,
---     board_id CHAR NOT NULL,
---     user_id CHAR NOT NULL,
---     FOREIGN KEY(board_id) REFERENCES Boards(board_id),
---     FOREIGN KEY(user_id) REFERENCES Users(user_id)
--- );
+DROP TABLE IF EXISTS Boards;
+CREATE TABLE Boards (
+    board_id CHAR PRIMARY KEY,
+    board_name CHAR
+);
 
+DROP TABLE IF EXISTS Users_Boards;
+CREATE TABLE Users_Boards (
+    users_boards_id char PRIMARY KEY,
+    board_id CHAR NOT NULL,
+    user_id CHAR NOT NULL,
+    FOREIGN KEY(board_id) REFERENCES Boards(board_id),
+    FOREIGN KEY(user_id) REFERENCES Users(user_id)
+);
+
+DROP TABLE IF EXISTS Columns;
 Create table Columns (
     column_id int PRIMARY KEY UNIQUE,
     board_id int not null,
@@ -21,4 +33,16 @@ Create table Columns (
     REFERENCES Boards(board_id)
     ON DELETE CASCADE 
     ON UPDATE NO ACTION
+);
+
+DROP TABLE IF EXISTS Tasks;
+CREATE TABLE Tasks (
+    task_id CHAR PRIMARY KEY,
+    task_name CHAR(50) NOT NULL,
+    deadline SMALLDATETIME
+    is_done INTEGER,
+    task_description CHAR(100),
+    subtask_id CHAR,
+    FOREIGN KEY(subtask_id)
+    REFERENCES Tasks(task_id)
 );
