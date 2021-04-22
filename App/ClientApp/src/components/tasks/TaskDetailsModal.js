dc8a25735cb9a1cb56275879206cc4ce6df84403
Zsani <horvathzsanett.95@gmail.com>
2021-04-22 17:42:32 +0200

    Filled modal window with data

--- App/ClientApp/src/components/tasks/TaskDetailsModal.js
+++ App/ClientApp/src/components/tasks/TaskDetailsModal.js
@@ -4,6 +4,8 @@ import { TaskDetailsContext } from "../contexts/TaskDetailsContext";
 
 function TaskDetailsModal() {
     const [taskDetails, setTaskDetails] = useContext(TaskDetailsContext);
+    const deadlineString = taskDetails.deadline;
+    const deadline = typeof deadlineString === "string" ? deadlineString.split('T')[0] : "";
 
     const closeModalWindow = () => {
         setTaskDetails([]);
@@ -11,8 +13,19 @@ function TaskDetailsModal() {
 
     return (
         <section>
-            <Modal className="create-modal" visible={ taskDetails.length == 0 ? false : true } width="400" height="250" effect="fadeInDown" onClickAway={() => closeModalWindow()}>
-                <div> {taskDetails.taskName} </div>
+            <Modal className="create-modal" visible={taskDetails.length == 0 ? false : true} width="400" height="250" effect="fadeInDown" onClickAway={() => closeModalWindow()}>
+                <div>
+                    <label><strong>Task name:</strong></label>
+                    <p> {taskDetails.taskName} </p>
+                </div>
+                <div>
+                    <label><strong>Deadline:</strong></label>
+                    <p> {deadline} </p>
+                </div>
+                <div>
+                    <label><strong>Description:</strong></label>
+                    <p> {taskDetails.description} </p>
+                </div>
             </Modal>
         </section>
         )
