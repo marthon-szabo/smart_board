58a1f91d6d6544701b8f92f07a9430a988455c0e
Zsani <horvathzsanett.95@gmail.com>
2021-04-29 13:28:50 +0200

    Added extra data for task details

--- App/ClientApp/src/components/tasks/TaskDetailsModal.js
+++ App/ClientApp/src/components/tasks/TaskDetailsModal.js
@@ -1,9 +1,16 @@
-﻿import React, { useContext } from 'react';
+﻿import React, { useContext, useState } from 'react';
 import Modal from 'react-awesome-modal';
+import Switch from "react-switch";
 import { TaskDetailsContext } from "../contexts/TaskDetailsContext";
+
+import "./TaskDetailsModal.scss";
 
 function TaskDetailsModal() {
     const [taskDetails, setTaskDetails] = useContext(TaskDetailsContext);
+    const [checked, setChecked] = useState(taskDetails.isDone);
+    const handleChange = nextChecked => {
+        setChecked(nextChecked);
+    };
 
     const closeModalWindow = () => {
         setTaskDetails([]);
@@ -11,8 +18,44 @@ function TaskDetailsModal() {
 
     return (
         <section>
-            <Modal className="create-modal" visible={ taskDetails.length == 0 ? false : true } width="400" height="250" effect="fadeInDown" onClickAway={() => closeModalWindow()}>
-                <div> {taskDetails.taskName} </div>
+            <Modal className="create-modal" visible={ taskDetails.length == 0 ? false : true } width="400" height="350" effect="fadeInDown" onClickAway={() => closeModalWindow()}>
+                <div>
+                    <label>
+                        Taskname:
+                    </label>
+                    <p>
+                        {taskDetails.taskName}
+                    </p>
+                </div>
+                <div>
+                    <label>
+                        Deadline:
+                    </label>
+                    <p>
+                        {taskDetails.deadline}
+                    </p>
+                </div>
+                <div>
+                    <label>
+                        Description:
+                    </label>
+                    <p>
+                        {taskDetails.description}
+                    </p>
+                </div>
+                <div>
+                    <label>
+                        Finished:
+                    </label>
+                    <p>
+                        {checked ? "yes" : "no"}
+                        <Switch
+                            onChange={handleChange}
+                            checked={checked}
+                            className="react-switch"
+                        />
+                    </p>
+                </div>
             </Modal>
         </section>
         )
