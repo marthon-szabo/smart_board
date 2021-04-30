d110573fb81ed8ec6282f41a7e9f3c0f2af9831f
Zsani <horvathzsanett.95@gmail.com>
2021-04-30 11:13:08 +0200

    Added functionality for double clicking a detail

--- App/ClientApp/src/components/tasks/TaskDetailsModal.js
+++ App/ClientApp/src/components/tasks/TaskDetailsModal.js
@@ -1,19 +1,115 @@
 ﻿import React, { useContext, useState } from 'react';
 import Modal from 'react-awesome-modal';
 import Switch from "react-switch";
+import Calendar from 'react-calendar';
 import { TaskDetailsContext } from "../contexts/TaskDetailsContext";
 
 import "./TaskDetailsModal.scss";
+import 'react-calendar/dist/Calendar.css';
+import Task from './Task';
 
 function TaskDetailsModal() {
     const [taskDetails, setTaskDetails] = useContext(TaskDetailsContext);
+    
+    const [changeNameState, setChangeNameState] = useState(false);
+    const [changeDateState, setChangeDateState] = useState(false);
+    const [changeDescriptionState, setChangeDescriptionState] = useState(false);
+    const [value, onChange] = useState(new Date());
     const [checked, setChecked] = useState(taskDetails.isDone);
+
     const handleChange = nextChecked => {
         setChecked(nextChecked);
     };
 
     const closeModalWindow = () => {
         setTaskDetails([]);
+    }
+
+    const switchToInput = (event, task) => {
+        const title = event.target;
+        const titleName = title.innerHTML;
+
+        const inputField = document.createElement("input")
+        inputField.setAttribute("type", "text");
+        inputField.value = titleName;
+        inputField.className = "column-input";
+
+        document.addEventListener("click", () => {
+            title.innerHTML = titleName;
+        });
+
+        inputField.addEventListener("keyup", (event) => {
+            if (event.keyCode === 13) {
+                title.innerHTML = inputField.value;
+            }
+        });
+
+        title.innerHTML = "";
+        title.appendChild(inputField);
+        inputField.focus();
+        inputField.select();
+    }
+
+    const changeTaskName = (e) => {
+        setChangeNameState(true);
+
+        const inputField = document.getElementById("changed-taskname");
+        console.log(inputField);
+
+        document.addEventListener("keyup", (event) => {
+            if (event.keyCode === 13) {
+                console.log(taskDetails);
+                const newData = {
+                    taskId: taskDetails.taskId,
+                    columnId: taskDetails.columnId,
+                    taskName: inputField.value,
+                    deadline: taskDetails.deadline,
+                    isDone: taskDetails.isDone,
+                    description: taskDetails.description
+                };
+                setTaskDetails(newData);
+                setChangeNameState(false);
+            }
+        });
+    }
+
+    const changeDeadline = (e) => {
+        setChangeDateState(true);
+        const inputField = document.getElementById("changed-date");
+        inputField.addEventListener("keyup", (event) => {
+            if (event.keyCode === 13) {
+                const newData = {
+                    taskId: taskDetails.taskId,
+                    columnId: taskDetails.columnId,
+                    taskName: taskDetails.taskName,
+                    deadline: value.toString(),
+                    isDone: taskDetails.isDone,
+                    description: taskDetails.description
+                };
+                setTaskDetails(newData);
+                setChangeDateState(false);
+            }
+        });
+    }
+
+    const changeDescription = (e) => {
+        setChangeDescriptionState(true);
+        
+        document.addEventListener("keyup", (event) => {
+            if (event.keyCode === 13) {
+                const inputField = document.getElementById("changed-description");
+                const newData = {
+                    taskId: taskDetails.taskId,
+                    columnId: taskDetails.columnId,
+                    taskName: taskDetails.taskName,
+                    deadline: taskDetails.deadline,
+                    isDone: taskDetails.isDone,
+                    description: inputField.value
+                };
+                setTaskDetails(newData);
+                setChangeDescriptionState(false);
+            }
+        });
     }
 
     return (
@@ -23,25 +119,61 @@ function TaskDetailsModal() {
                     <label>
                         Taskname:
                     </label>
-                    <p>
-                        {taskDetails.taskName}
-                    </p>
+                    {
+                        changeNameState === true &&
+                        <div>
+                            <input id="changed-taskname" type="text"></input>
+                        </div>
+                    }
+                    {
+                        changeNameState === false &&
+                        <p onDoubleClick={(e) => changeTaskName(e)}
+                            title="Double click to change">
+                            {taskDetails.taskName}
+                        </p>
+                    }
+                    
                 </div>
                 <div>
                     <label>
                         Deadline:
                     </label>
-                    <p>
-                        {taskDetails.deadline}
-                    </p>
+                    {
+                        changeDateState === true &&
+                        <div>
+                            <Calendar
+                                id="changed-date"
+                                onChange={onChange}
+                                value={value}
+                                locale="en-EN"
+                            />
+                        </div>
+                    }
+                    {
+                        changeDateState === false &&
+                        <p onDoubleClick={(e) => changeDeadline(e)}
+                            title="Double click to change">
+                            {taskDetails.deadline}
+                        </p>
+                    }
                 </div>
                 <div>
                     <label>
                         Description:
                     </label>
-                    <p>
-                        {taskDetails.description}
-                    </p>
+                    {
+                        changeDescriptionState === true &&
+                        <div>
+                            <textarea id="changed-description" style={{ height: "105px" }}></textarea>
+                        </div>
+                    }
+                    {
+                        changeDescriptionState === false &&
+                        <p onDoubleClick={(e) => changeDescription(e)}
+                            title="Double click to change">
+                            {taskDetails.description}
+                        </p>
+                    }
                 </div>
                 <div>
                     <label>
