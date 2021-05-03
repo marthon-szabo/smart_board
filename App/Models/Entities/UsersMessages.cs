2da3941ca3abf1c6c7a6df41926544528515dbf7
marthon-szabo <makotoimx@gmail.com>
2021-05-03 14:23:54 +0200

    Create UsersMessages join model

---
+++ App/Models/Entities/UsersMessages.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace App.Models.Entities
+{
+    [Table("Users_Messages")]
+    public class UsersMessages
+    {
+        [Column("id")]
+        [Required]
+        public string Id { get; set; }
+
+        [Column("user_id")]
+        [Required]
+        public string UserId { get; set; }
+
+        [Column("message_id")]
+        [Required]
+        public string MessageId { get; set; }
+        
+        
+        
+    }
+}
