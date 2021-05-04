10f4713a32e3814c11c3df5c837df1b78e30de5b
Zsani <horvathzsanett.95@gmail.com>
2021-05-04 12:10:16 +0200

    Created context for selected available quest's details

---
+++ App/ClientApp/src/components/contexts/questContexts/AvailableQuestDetailContext.js
@@ -0,0 +1,13 @@
+﻿import React, { useState, createContext } from 'react';
+
+export const AvailableQuestDetailContext = createContext();
+
+export const AvailableQuestDetailProvider = props => {
+    const [availableQuestState, setAvailableQuestState] = useState([]);
+
+    return (
+        <AvailableQuestDetailContext.Provider value={[availableQuestState, setAvailableQuestState]}>
+            {props.children}
+        </AvailableQuestDetailContext.Provider>
+    )
+}
