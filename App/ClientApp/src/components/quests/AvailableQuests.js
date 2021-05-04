4e6a75f848b3633aa0835a468b02e81b25cd50ed
Zsani <horvathzsanett.95@gmail.com>
2021-05-04 10:13:16 +0200

    Created object for storing information about available quests

--- App/ClientApp/src/components/quests/AvailableQuests.js
+++ App/ClientApp/src/components/quests/AvailableQuests.js
@@ -11,54 +11,61 @@ function showElements(quest) {
         <div>
             <AvailableQuest quest={quest} />
         </div>
-        )}
+    )
+}
 
-function AvailableQuests () {
-    const availableQuestList = [{
-        picturePath: "/images/quests/timer.png" 
+function AvailableQuests() {
+    const availableQuestList = [
+        {
+            picturePath: "/images/quests/timer.png",
+            description: "Finish 5 tasks in a week to earn this badge!"
         },
         {
-            picturePath: "/images/quests/chest.png"
+            picturePath: "/images/quests/chest.png",
+            description: "Make 3 new friends this week to earn this badge!"
         },
         {
-            picturePath: "/images/quests/exclamation.png"
+            picturePath: "/images/quests/exclamation.png",
+            description: "Create a new board with a friend to earn this badge!"
         },
         {
-            picturePath: "/images/quests/pen.png"
+            picturePath: "/images/quests/pen.png",
+            description: "Create 10 tasks in a week to earn this badge!"
         },
         {
-            picturePath: "/images/quests/sandclock.png"
+            picturePath: "/images/quests/sandclock.png",
+            description: "Finish 10 tasks in a week to earn this badge!"
         },
         {
-            picturePath: "/images/quests/star.png"
+            picturePath: "/images/quests/star.png",
+            description: "Make a new friend this week to earn this badge!"
         }]
 
 
-        const settings = {
-            dots: true,
-            infinite: true,
-            speed: 500,
-            slidesToShow: 3,
-            slidesToScroll: 1
-        };
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1
+    };
 
-        const centerStyle = {
-            diplay: "block",
-            left: "auto",
-            right: "auto"
-        };
+    const centerStyle = {
+        diplay: "block",
+        left: "auto",
+        right: "auto"
+    };
 
-        return (
+    return (
 
-            <div style={{ width: "auto", marginLeft:"35px", marginRight: "35px" }}>
-                <p style={ centerStyle }>Available quests</p>
-                <Slider {...settings}>
-                    {availableQuestList.map((quest) => showElements(quest))}
-                </Slider>
-                
-            </div>
-        );
-    
+        <div style={{ width: "auto", marginLeft: "35px", marginRight: "35px" }}>
+            <p style={centerStyle}>Available quests</p>
+            <Slider {...settings}>
+                {availableQuestList.map((quest) => showElements(quest))}
+            </Slider>
+
+        </div>
+    );
 }
 
 export default AvailableQuests;
