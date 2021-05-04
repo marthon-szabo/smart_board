8e37c65f8c677915f1a1851e9822325a8fc8b3c2
Zsani <horvathzsanett.95@gmail.com>
2021-05-04 13:58:12 +0200

    Extended AvailableQUestList's details with task names and limit times

--- App/ClientApp/src/components/quests/AvailableQuests.js
+++ App/ClientApp/src/components/quests/AvailableQuests.js
@@ -18,33 +18,45 @@ function AvailableQuests() {
     const availableQuestList = [
         {
             id: 1,
+            questName: "In time finisher",
             picturePath: "/images/quests/timer.png",
-            description: "Finish 5 tasks in a week to earn this badge!"
+            description: "Finish 5 tasks in a week to earn this badge!",
+            toFinish: "7 days"
         },
         {
             id: 2,
+            questName: "Friend collector",
             picturePath: "/images/quests/chest.png",
-            description: "Make 3 new friends this week to earn this badge!"
+            description: "Make 3 new friends this week to earn this badge!",
+            toFinish: "7 days"
         },
         {
             id: 3,
+            questName: "Important teamworker",
             picturePath: "/images/quests/exclamation.png",
-            description: "Create a new board with a friend to earn this badge!"
+            description: "Create a new board with a friend to earn this badge!",
+            toFinish: "7 days"
         },
         {
             id: 4,
+            questName: "Creative creator",
             picturePath: "/images/quests/pen.png",
-            description: "Create 10 tasks in a week to earn this badge!"
+            description: "Create 10 tasks in a week to earn this badge!",
+            toFinish: "7 days"
         },
         {
             id: 5,
+            questName: "Task master",
             picturePath: "/images/quests/sandclock.png",
-            description: "Finish 10 tasks in a week to earn this badge!"
+            description: "Finish 10 tasks in a week to earn this badge!",
+            toFinish: "7 days"
         },
         {
             id: 6,
+            questName: "Friend maker superstar",
             picturePath: "/images/quests/star.png",
-            description: "Make a new friend this week to earn this badge!"
+            description: "Make a new friend this week to earn this badge!",
+            toFinish: "7 days"
         }]
 
     const settings = {
