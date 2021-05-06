358b39163f4a35721bd6000f71c33ae647225648
Zsani <horvathzsanett.95@gmail.com>
2021-05-06 13:59:08 +0200

    Created validation for password changing

---
+++ App/ClientApp/src/components/profile/ProfileUseForm.js
@@ -0,0 +1,32 @@
+﻿import { useState } from 'react';
+
+const ProfileUseForm = (callback, validate) => {
+    const [values, setValues] = useState({
+        oldPassword: '',
+        newPassword: '',
+        confirmedPassword: ''
+    });
+
+    const [errors, setErrors] = useState({});
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        console.log(name);
+        console.log(value);
+        setValues({
+            ...values,
+            [name]: value
+        });
+        console.log(values);
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setErrors(validate(values));
+    };
+
+    return { handleChange, values, handleSubmit, errors };
+}
+
+export default ProfileUseForm;
