9b5658023de9f63f122d4a7efddd381e4d9eacbe
marthon-szabo <makotoimx@gmail.com>
2021-05-11 10:59:45 +0200

    Refactor IBoardRepository

--- App/Services/Repositories/Interfaces/IBoardRepository.cs
+++ App/Services/Repositories/Interfaces/IBoardRepository.cs
@@ -5,7 +5,7 @@ namespace App.Services.Repositories.Interfaces
 {
     public interface IBoardRepository : IGeneralRepository<Board>
     {
-        IEnumerable<Board> GetAllBoardsByUsername(string username);
+        IEnumerable<Board> GetAllBoardsByUserId(string iserId);
 
         public Board? GetBoardByBoardName(string boardName);
     }
