de2e569e6fcc87ec98e4d506ed1817489ddc9179
marthon-szabo <makotoimx@gmail.com>
2021-05-11 10:52:09 +0200

    Add UserName field for UserProfileVM model

--- App/Models/ViewModels/UserProfileVM.cs
+++ App/Models/ViewModels/UserProfileVM.cs
@@ -2,7 +2,7 @@ namespace App.Models.ViewModels
 {
     public class UserProfileVM
     {
-        public string Username { get; set; }
+        public string UserName { get; set; }
         public string Email { get; set; }
         public string TakenQuests { get; set; }
         public string DoneQuests { get; set; }
