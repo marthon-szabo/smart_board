ce69d07b6be9f74d6c81718a7d507fb66a528898
marthon-szabo <makotoimx@gmail.com>
2021-05-11 10:57:24 +0200

    Extend IChatClient interface with methods

--- App/Services/Hubs/Interfaces/IChatClient.cs
+++ App/Services/Hubs/Interfaces/IChatClient.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using App.Models.Entities;
 
 namespace App.Services.Hubs.Interfaces
@@ -5,5 +6,8 @@ namespace App.Services.Hubs.Interfaces
     public interface IChatClient
     {
         System.Threading.Tasks.Task ReceiveMessage(ChatMessage message);
+        System.Threading.Tasks.Task SendAsync(string group, ChatMessage message);
+        System.Threading.Tasks.Task SendMessagesAsync(string group, IEnumerable<ChatMessage> message);
+        System.Threading.Tasks.Task JoinGroup(string group);
     }
 }
