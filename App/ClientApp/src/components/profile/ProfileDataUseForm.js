6b791107152a91256b28c57e8370796a3a538d24
Zsani <horvathzsanett.95@gmail.com>
2021-05-12 14:13:58 +0200

    Modified the validate function's parameters for proper validation

---
+++ App/ClientApp/src/components/profile/ProfileDataUseForm.js
@@ -0,0 +1,30 @@
+﻿import { useState, useContext } from 'react';
+import { UserDataContext } from "../contexts/userContexts/UserDataContext";
+
+const ProfileDataUseForm = (validate) => {
+    const [valuesData, setValuesData] = useState({
+        newUsername: '',
+        newEmail: ''
+    });
+
+    const [userData, setUserData] = useContext(UserDataContext);
+    const [errorsData, setErrorsData] = useState({});
+
+    const handleChangeData = e => {
+        const { name, value } = e.target;
+        setValuesData({
+            ...valuesData,
+            [name]: value
+        });
+    };
+
+    const handleSubmitData = (e) => {
+        e.preventDefault();
+        setErrorsData(validate(valuesData, userData.username, userData.email));
+        console.log(errorsData);
+    };
+
+    return { handleChangeData, valuesData, handleSubmitData, errorsData };
+}
+
+export default ProfileDataUseForm;
