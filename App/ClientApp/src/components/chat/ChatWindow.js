225fb7241cceb47e63181b250ceffff4bc070444
marthon-szabo <makotoimx@gmail.com>
2021-05-12 09:30:08 +0200

    Refactor chat window component
    Implement toggle logic and set chat input component inside it

--- App/ClientApp/src/components/chat/ChatWindow.js
+++ App/ClientApp/src/components/chat/ChatWindow.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 import Message from './Message';
+import ChatInput from './ChatInput';
 
 const ChatWindow = (props) => {
+    
     const chat = props.chat
         .map(m => <Message 
             key={Date.now() * Math.random()}
@@ -10,8 +12,9 @@ const ChatWindow = (props) => {
             message={m.content}/>);
 
     return(
-        <div>
+        <div hidden={props.isHidden}>
             {chat}
+            <ChatInput sendMessage={props.sendMessage} />
         </div>
     )
 };
