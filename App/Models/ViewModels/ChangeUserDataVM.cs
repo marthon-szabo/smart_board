f125d99d14f4624a2ee573d1ef3fe847b0c1d575
Zsani <horvathzsanett.95@gmail.com>
2021-05-12 14:56:03 +0200

    Created ViewModel for new user data

---
+++ App/Models/ViewModels/ChangeUserDataVM.cs
@@ -0,0 +1,9 @@
+﻿namespace App.Models.ViewModels
+{
+    public class ChangeUserDataVM
+    {
+        public string Username { get; set; }
+        public string NewUsername { get; set; }
+        public string NewEmail { get; set; }
+    }
+}
