f6a0b4a2323c1e6fba24960536657f59a99984a8
Zsani <horvathzsanett.95@gmail.com>
2021-05-14 13:54:58 +0200

    Created context for storing all available users that can be added as a contributor for boards

---
+++ App/ClientApp/src/components/contexts/userContexts/AddUserToBoardContext.js
@@ -0,0 +1,13 @@
+﻿import React, { useState, createContext } from 'react';
+
+export const UserDataContext = createContext();
+
+export const AddUserToBoardProvider = props => {
+    const [availableUsersState, setAvailableUsersState] = useState([]);
+
+    return (
+        <AddUserToBoardContext.Provider value={[availableUsersState, setAvailableUsersState]}>
+            {props.children}
+        </AddUserToBoardContext.Provider>
+    )
+}
