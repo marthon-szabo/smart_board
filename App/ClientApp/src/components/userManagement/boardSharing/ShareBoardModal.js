bb2f0c8f13c443c2d64ca323664f4c70220d4266
Zsani <horvathzsanett.95@gmail.com>
2021-05-18 11:45:31 +0200

    Added content with dropdown menu to modal window

--- App/ClientApp/src/components/userManagement/boardSharing/ShareBoardModal.js
+++ App/ClientApp/src/components/userManagement/boardSharing/ShareBoardModal.js
@@ -1,8 +1,43 @@
-﻿import React from 'react';
+﻿import React, { useContext, useState } from 'react';
+import Modal from 'react-awesome-modal';
+import { AddUserToBoardContext } from "../../contexts/userContexts/AddUserToBoardContext";
 
 function ShareBoardModal() {
+
+    const [shareBoardState, setShareBoardState] = useContext(AddUserToBoardContext);
+    const [selectedUser, setSelectedUser] = useState("");
+
+    const closeShareWindow = () => {
+        setShareBoardState([]);
+    }
+
+    function showUsers(username) {
+        return (<option value={username}>{username}</option>)
+    }
+
+    const saveChange = (e) => {
+        setSelectedUser(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(selectedUser);
+    }
+
     return (
-        <p>Modal will come here</p>
+        <section>
+            <Modal className="share-board-modal" visible={shareBoardState.length == 0 ? false : true} width="400" height="350" effect="fadeInDown" onClickAway={() => closeShareWindow()}>
+                <p>Select a user to add as a contributor to this board:</p>
+                <form onSubmit={(e) => handleSubmit(e)}>
+                    <select onChange={(e) => saveChange(e)}>
+                        {shareBoardState.map(showUsers)}
+                    </select>
+                    <button id="send-new-user-btn" type="submit" className="btn btn-primary btn-block">
+                        Add user
+                    </button>
+                </form>
+            </Modal>
+        </section>
         )
 }
 
