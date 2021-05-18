13642c015f3bc3f22002d0ee3353869cf9158da3
marthon-szabo <makotoimx@gmail.com>
2021-05-18 11:20:45 +0200

    Add sender name property

--- App/ClientApp/src/components/chat/ChatWindow.js
+++ App/ClientApp/src/components/chat/ChatWindow.js
@@ -4,11 +4,12 @@ import Message from './Message';
 import ChatInput from './ChatInput';
 
 const ChatWindow = (props) => {
-    
+
     const chat = props.chat
         .map(m => <Message 
             key={Date.now() * Math.random()}
             profilePicture={m.profilePicture}
+            name={m.senderName}
             message={m.content}/>);
 
     return(
