38e306a5fe5dd22b49539f25e61d118ccf45274b
marthon-szabo <makotoimx@gmail.com>
2021-05-18 11:21:05 +0200

    Add profile picture property

--- App/ClientApp/src/components/chat/Message.js
+++ App/ClientApp/src/components/chat/Message.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Message = (props) =>{
 
+    console.log(`props: `, props)
+
     return (
     <div style={{ background: "#eee", borderRadius: '5px', padding: '0 10px' }}>
-        <p><img className="chat-client__profile-picture"src={props.profilePicture} /></p>
+        <p><img className="chat-client__profile-picture" src={props.profilePicture} /></p>{props.name}:
         <p>{props.message}</p>
     </div>
     );
